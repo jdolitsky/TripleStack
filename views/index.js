a3acec3946b4bbcfbda608fdf5eaef5cbd74b46d
Josh Dolitsky <jdolitsky@gmail.com>
2012-07-24 18:06:21 -0400

    worked with MVC architecture

--- views/index.js
+++
@@ -1,9 +0,0 @@
-<?js
-
-var text1 = 'Welcome to TripleStack';
-var text2 = 'Try Me';
-
-?>
-<h1><?js spit(text1);?></h1>
-<div id="myButton"><div id="bText"><?js spit(text2);?></div></div>
-<div id="log"></div>
--- views/top.js
+++
@@ -1,7 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head>
-<title>TripleStack</title>{3S-CSS}
-</head>
-<body>
-<div id="content">
